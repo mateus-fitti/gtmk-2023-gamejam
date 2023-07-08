4eeac47c977057413a79189690a046be142c1322
Eduardo Santos <duduroque08@gmail.com>
2023-07-08 15:15:26 -0300

    Adicionar Sistema de Som
    Adicionei o Sound Manager e coloquei uma musica teste para tocar

---
+++ ReverseProject2D/Assets/Scripts/Utils/Sound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+[Serializable]
+public class Sound
+{
+    [Tooltip("Nome do áudio")]
+    public string name;
+    [Tooltip("Arraste o áudio correspondente para aqui")]
+    public AudioClip clip;
+    [Tooltip("Volume do áudio")]
+    [Range(0f, 1.2f)]
+    public float volume = .75f;
+    [Tooltip("Altura do áudio")]
+    [Range(.1f, 3f)]
+    public float pitch = 1f;
+    [Tooltip("Marque se o áudio deve se repetir")]
+    public bool loop;
+    [Tooltip("Marque se apenas este áudio deve ser tocado quando acionado")]
+    public bool single = false;
+
+    [HideInInspector]
+    public AudioSource source;
+    [HideInInspector]
+    public AudioMixerGroup mixer;
+}
---
+++ ReverseProject2D/Assets/Scripts/Utils/SoundManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioMixerGroup mixer;
+    public List<Sound> sounds;
+    
+    public static SoundManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = mixer;
+        }
+
+        Play("Test");
+    }
+
+    public void Play (string name)
+    {
+        Sound s = sounds.Find(sound => sound.name == name);
+        if (s == null) return;
+
+        s.source.volume = s.volume;
+        s.source.pitch = s.pitch;
+
+        if (s.single) StopAllSounds();
+        
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s =sounds.Find(sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        } 
+        s.source.Stop();
+    }
+
+
+    void StopAllSounds()
+    {
+        foreach (Sound s in sounds)
+        {
+            if (s == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found!");
+                return;
+            }
+            s.source.Stop();
+        }
+    }
+
+}
