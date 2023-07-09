69027c0ddc3641feb4171abc44b20269fe9c976a
Mateus Fittipaldi <mateusfitti@gmail.com>
2023-07-09 03:54:32 -0300

    Add Sound assets and Update MainMenu
    Adicionado assets de música e efeitos sonoros
    Atualizado o MainMenu, agora carrega a Main scene com o level principal

---
+++ ReverseProject2D/Assets/Scripts/GameController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void OnSceneButton(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
